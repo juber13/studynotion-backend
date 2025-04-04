9c98a4cf5dbb047f3fdfb9bda0bf7be6db90c1a8
juber <juberk1010@gmail.com>
2025-04-04 16:49:36 +0530

    make origin global

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173/", // This line allows requests from any origin (domain) to access the API
+    origin: "*", // This line allows requests from any origin (domain) to access the API
     credentials: true,
   })
 );
